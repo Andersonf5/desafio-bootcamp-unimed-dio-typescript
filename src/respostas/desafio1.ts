983c9050de558b2803ddb14aadec64cc440e18af
Andersonf5 <anderson.silvab@gmail.com>
2022-06-23 14:56:04 -0300

    Desafio typescript do 1 ao 3

---
+++ src/respostas/desafio1.ts
@@ -0,0 +1,21 @@
+
+
+//Resposta 1
+
+interface Employee{
+    code:number,
+    name:string
+}
+ 
+let employee:Employee = {
+    code:10,
+    name:'João'
+}
+
+//Resposta 2
+
+let employee1 = {
+code: 10,
+name:"John"
+};
+
